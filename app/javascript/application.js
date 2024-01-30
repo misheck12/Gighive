eadb07923076e04fe6f71ac2c8643350378f7e70
Misheck12 <Misheck1720@gmail.com>
2024-01-30 11:34:09 +0000

    Jan 30, 2024, 1:34 PM

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,7 @@
+// Entry point for the build script in your package.json
+import "@hotwired/turbo-rails";
+import "./controllers";
+import Rails from "@rails/ujs";
 
-import "@hotwired/turbo-rails"
-import "./controllers"
-import * as Bootstrap from "bootstrap"
-import Rails from "@rails/ujs"
-
-Rails.start()import * as bootstrap from "bootstrap"
+Rails.start(); // Add a semicolon here
+import * as Bootstrap from "bootstrap"; // Use the pinned name here
