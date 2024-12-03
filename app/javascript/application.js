8fe08e573262167e88e040608be61ffc58ccdd98
Misheck12 <Misheck1720@gmail.com>
2024-12-03 22:59:24 +0000

    Dec 4, 2024, 12:59 AM

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,6 @@
-
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as Bootstrap from "bootstrap"
 import Rails from "@rails/ujs"
 
-Rails.start()import * as bootstrap from "bootstrap"
+Rails.start()
