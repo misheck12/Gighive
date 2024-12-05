67750971a94624747f4ba2e4ee5082dcba0e0e57
Misheck12 <Misheck1720@gmail.com>
2024-12-05 09:33:22 +0000

    Dec 5, 2024, 11:33 AM

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
-require_relative "boot"
+require_relative 'boot'
 
-require "rails/all"
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -9,18 +9,9 @@ Bundler.require(*Rails.groups)
 module Gighive
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.0
+    config.load_defaults 6.1
 
-    # Configuration for the application, engines, and railties goes here.
-     # Load environment variables from .env file 
-     Dotenv::Railtie.load if defined?(Dotenv)
-    
-    # Use Sidekiq for Active Job 
-    config.active_job.queue_adapter = :sidekiq
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    # Add this line to use dotenv-rails
+    Dotenv::Rails.load if Rails.env.development? || Rails.env.test?
   end
 end
