0787753f9588e9c6edfda2bfbf8fed7425780f8b
Misheck12 <Misheck1720@gmail.com>
2024-12-05 08:50:23 +0000

    Dec 5, 2024, 10:50 AM

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -1,34 +1,35 @@
 # frozen_string_literal: true
 
 class Users::PasswordsController < Devise::PasswordsController
-  # GET /resource/password/new
-  # def new
-  #   super
-  # end
+  # GET /users/password/new
+  def new
+    super
+  end
 
-  # POST /resource/password
-  # def create
-  #   super
-  # end
+  # POST /users/password
+  def create
+    super
+  end
 
-  # GET /resource/password/edit?reset_password_token=abcdef
-  # def edit
-  #   super
-  # end
+  # GET /users/password/edit?reset_password_token=abcdef
+  def edit
+    super
+  end
 
-  # PUT /resource/password
-  # def update
-  #   super
-  # end
+  # PUT /users/password
+  def update
+    super
+  end
 
-  # protected
+  protected
 
-  # def after_resetting_password_path_for(resource)
-  #   super(resource)
-  # end
+  # After resetting the password, redirect to a custom path
+  def after_resetting_password_path_for(resource)
+    signed_in_root_path(resource)
+  end
 
   # The path used after sending reset password instructions
-  # def after_sending_reset_password_instructions_path_for(resource_name)
-  #   super(resource_name)
-  # end
+  def after_sending_reset_password_instructions_path_for(resource_name)
+    new_session_path(resource_name)
+  end
 end
