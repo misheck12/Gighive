915d33fe6d914aeb7c1e1f15a4863d930b02e220
Misheck12 <Misheck1720@gmail.com>
2024-12-27 08:23:03 +0000

    Dec 27, 2024, 10:23 AM

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,9 +1,6 @@
 class Task < ApplicationRecord
-  # Validations
-  validates :title, presence: true, length: { minimum: 5, maximum: 100 }
-  validates :description, presence: true, length: { minimum: 10 }
-  validates :budget, presence: true, numericality: { greater_than: 0 }
-  validates :deadline, presence: true
+  belongs_to :client, class_name: 'User'
+  belongs_to :freelancer, class_name: 'User', optional: true
 
   has_one_attached :completed_file
   has_one_attached :revised_file
@@ -12,15 +9,20 @@ class Task < ApplicationRecord
   # Enum for status
   enum status: { open: 0, in_progress: 1, changes_requested: 2, completed: 3 }
 
+  # Validations
+  validates :title, presence: true, length: { minimum: 5, maximum: 100 }
+  validates :description, presence: true, length: { minimum: 10 }
+  validates :pages, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :deadline, presence: true
+
   # Callback to set default status
   before_validation :set_default_status, on: :create
 
   # Scopes
-  scope :open_tasks, -> { where(status: :open) } # Note: Use symbol :open instead of string 'open'
+  scope :open_tasks, -> { where(status: :open) }
 
   # Associations
-  belongs_to :client, class_name: 'User'
-  belongs_to :freelancer, class_name: 'User', optional: true
   has_many :reviews, dependent: :destroy
   has_one :payment, dependent: :destroy
 
@@ -28,9 +30,8 @@ class Task < ApplicationRecord
     reviews.exists?
   end
 
-  def self.total_earning_for_freelancer (user_id)
-  joins(:payment).where(freelancer_id: user_id, payments: {status: :approved})
-
+  def self.total_earning_for_freelancer(user_id)
+    joins(:payment).where(freelancer_id: user_id, payments: { status: :approved })
   end
 
   private
