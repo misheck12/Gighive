60d5f94e2151b091c8b6a60567204fbc437385f8
Misheck12 <Misheck1720@gmail.com>
2024-12-27 10:17:15 +0200

    update files

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,6 +6,3 @@ pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "bootstrap", to: "bootstrap.min.js"
-# config/importmap.rb
-
-pin "tasks_form", to: "tasks_form.js"
