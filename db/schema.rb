619bb57a962fbdbd4aee2c8d3343fce46cc5e35e
Misheck12 <Misheck1720@gmail.com>
2025-01-02 11:54:29 +0000

    Jan 2, 2025, 1:54 PM

--- db/schema.rb
+++ db/schema.rb
@@ -81,12 +81,6 @@ ActiveRecord::Schema[7.0].define(version: 2025_01_02_115004) do
     t.index ["category_id"], name: "index_subcategories_on_category_id"
   end
 
-  create_table "task_categories", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -98,8 +92,6 @@ ActiveRecord::Schema[7.0].define(version: 2025_01_02_115004) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "category"
-    t.string "reference_no"
-    t.string "transaction_id"
     t.bigint "category_id"
     t.bigint "subcategory_id"
     t.index ["category_id"], name: "index_tasks_on_category_id"
