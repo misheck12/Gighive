57fc536ad76f4e407f01de1d08796ea15c9d4718
Misheck12 <Misheck1720@gmail.com>
2025-01-02 13:16:20 +0000

    Jan 2, 2025, 3:16 PM

--- app/controllers/Categories_controller.rb
+++ app/controllers/Categories_controller.rb
@@ -1,11 +1,7 @@
 class CategoriesController < ApplicationController
-  before_action :authenticate_user!  # Ensure users are authenticated
-
   def subcategories
     category = Category.find(params[:id])
     subcategories = category.subcategories.select(:id, :name)
     render json: { subcategories: subcategories }
-  rescue ActiveRecord::RecordNotFound
-    render json: { error: 'Category not found' }, status: :not_found
   end
 end
