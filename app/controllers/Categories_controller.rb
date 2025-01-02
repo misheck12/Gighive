1341141f9ef92dadf303848f4d21d2a2a499d295
Misheck12 <Misheck1720@gmail.com>
2025-01-02 12:56:51 +0000

    Jan 2, 2025, 2:56 PM

--- app/controllers/Categories_controller.rb
+++ app/controllers/Categories_controller.rb
@@ -1,5 +1,5 @@
 class CategoriesController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!  # Ensure users are authenticated
 
   def subcategories
     category = Category.find(params[:id])
