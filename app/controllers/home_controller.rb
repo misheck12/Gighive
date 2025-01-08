6988a5970e9855372c896389d915399c9fcd36a4
Misheck12 <Misheck1720@gmail.com>
2025-01-08 14:39:24 +0000

    Jan 8, 2025, 4:39 PM

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,8 @@
 class HomeController < ApplicationController
+  # Skip authentication for the index action to allow public access to the landing page
+  skip_before_action :authenticate_user!, only: [:index]
+
   def index
+    # Any additional logic for the landing page can be added here
   end
 end
