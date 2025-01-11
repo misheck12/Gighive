b7ea149687fdea2d2550da0e736d7209fc8f523b
Misheck12 <Misheck1720@gmail.com>
2025-01-11 14:21:35 +0000

    Jan 11, 2025, 4:21 PM

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,14 @@
 class PagesController < ApplicationController
+
+  skip_before_action :authenticate_user!, only: [:about_us, :working_with_us]
+
   def about_us
   end
 
   def working_with_us
+  end
+
+  def contact_us
+    
   end
 end
