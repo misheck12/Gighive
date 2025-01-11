e50710414f95d7ebeba8f995c8217ca0aa9f7bc2
Misheck12 <Misheck1720@gmail.com>
2025-01-11 20:52:56 +0000

    Jan 11, 2025, 10:52 PM

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -95,7 +95,7 @@ class TasksController < ApplicationController
     base_price = task.subcategory&.minimum_price || 0
     complexity_addition = { 'Low' => 0, 'Medium' => 100, 'High' => 200 }[task.complexity] || 0
     urgency_addition = { 'Low' => 0, 'Normal' => 150, 'High' => 250 }[task.urgency] || 0
-    revisions_addition = (revisions.presence || 1) * 2
+    revisions_addition = (task.revisions.presence || 1) * 2
 
     (base_price + complexity_addition + urgency_addition + revisions_addition).round(2)
  end
