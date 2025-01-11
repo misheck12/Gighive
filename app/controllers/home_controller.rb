7b828e543663c6f68490e64c706f866c55f82060
Misheck12 <Misheck1720@gmail.com>
2025-01-11 13:44:19 +0000

    Jan 11, 2025, 3:44 PM

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,19 @@
 class HomeController < ApplicationController
   # Skip authentication for the index action to allow public access to the landing page
   skip_before_action :authenticate_user!, only: [:index]
+  
+  # Redirect logged-in users away from the home page
+  before_action :redirect_if_logged_in, only: [:index]
 
   def index
     # Any additional logic for the landing page can be added here
+  end
+
+  private
+
+  def redirect_if_logged_in
+    if user_signed_in?
+      redirect_to dashboard_path, notice: "You are already signed in."
+    end
   end
 end
