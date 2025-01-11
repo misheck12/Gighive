ff59693da712ee23511f8bea879f07f9119022a6
Misheck12 <Misheck1720@gmail.com>
2025-01-11 18:52:43 +0000

    Jan 11, 2025, 8:52 PM

--- app/mailers/task_mailer.rb
+++ app/mailers/task_mailer.rb
@@ -1,2 +1,35 @@
 class TaskMailer < ApplicationMailer
+  default from: 'bwangubwangu5@gmail.com'
+
+  def task_created(task)
+    @task = task
+    @client = task.client
+    mail(to: @client.email, subject: 'Your Task Has Been Created')
+  end
+
+  def task_accepted(task)
+    @task = task
+    @freelancer = task.freelancer
+    mail(to: @freelancer.email, subject: 'You Have Accepted a Task')
+  end
+
+  def task_completed(task)
+    @task = task
+    @client = task.client
+    @freelancer = task.freelancer
+    mail(to: @client.email, subject: 'Your Task Has Been Completed')
+  end
+
+  def changes_requested(task)
+    @task = task
+    @freelancer = task.freelancer
+    mail(to: @freelancer.email, subject: 'Changes Requested for Your Task')
+  end
+
+  def changes_submitted(task)
+    @task = task
+    @client = task.client
+    @freelancer = task.freelancer
+    mail(to: @client.email, subject: 'Changes Have Been Submitted for Your Task')
+  end
 end
