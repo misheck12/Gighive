1fb97c44cbb0cda4a918efb84ffe512fb2c60bd5
Misheck12 <Misheck1720@gmail.com>
2025-01-11 18:41:02 +0000

    Jan 11, 2025, 8:41 PM

--- app/mailers/payment_mailer.rb
+++ app/mailers/payment_mailer.rb
@@ -1,2 +1,24 @@
 class PaymentMailer < ApplicationMailer
+  default from: 'bwangubwangu5@gmail.com'
+
+  def payment_created(payment)
+    @payment = payment
+    @client = payment.task.client
+    @freelancer = payment.task.freelancer
+    mail(to: @client.email, subject: 'Payment Submitted for Your Task')
+  end
+
+  def payment_approved(payment)
+    @payment = payment
+    @client = payment.task.client
+    @freelancer = payment.task.freelancer
+    mail(to: @freelancer.email, subject: 'Your Payment Has Been Approved')
+  end
+
+  def payment_rejected(payment)
+    @payment = payment
+    @client = payment.task.client
+    @freelancer = payment.task.freelancer
+    mail(to: @client.email, subject: 'Your Payment Has Been Rejected')
+  end
 end
