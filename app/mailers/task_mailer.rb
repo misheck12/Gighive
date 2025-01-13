e9754637994d38022e4f33859b82ef5a93943837
Misheck12 <Misheck1720@gmail.com>
2025-01-13 14:48:13 +0000

    Jan 13, 2025, 4:48 PM

--- app/mailers/task_mailer.rb
+++ app/mailers/task_mailer.rb
@@ -7,29 +7,55 @@ class TaskMailer < ApplicationMailer
     mail(to: @client.email, subject: 'Your Task Has Been Created')
   end
 
-  def task_accepted(task)
+  def task_accepted_client(task)
+    @task = task
+    @client = task.client
+    @freelancer = task.freelancer
+    mail(to: @client.email, subject: "Your Task Has Been Accepted by #{@freelancer.name}")
+  end
+
+  def task_accepted_freelancer(task)
     @task = task
     @freelancer = task.freelancer
     mail(to: @freelancer.email, subject: 'You Have Accepted a Task')
   end
 
-  def task_completed(task)
+  def task_completed_client(task)
     @task = task
     @client = task.client
     @freelancer = task.freelancer
     mail(to: @client.email, subject: 'Your Task Has Been Completed')
   end
 
-  def changes_requested(task)
-    @task = task
-    @freelancer = task.freelancer
-    mail(to: @freelancer.email, subject: 'Changes Requested for Your Task')
+  def task_completed_freelancer(task)
+      @task = task
+      @freelancer = task.freelancer
+      mail(to: @freelancer.email, subject: 'You Have Completed a Task')
   end
 
-  def changes_submitted(task)
+  def changes_requested_client(task)
+    @task = task
+    @client = task.client
+    @freelancer = task.freelancer
+    mail(to: @client.email, subject: "Changes Have Been Requested for Your Task - #{@task.title}")
+  end
+
+  def changes_requested_freelancer(task)
+    @task = task
+    @freelancer = task.freelancer
+    mail(to: @freelancer.email, subject: "Changes Requested for Task - #{@task.title}")
+  end
+
+  def changes_submitted_client(task)
     @task = task
     @client = task.client
     @freelancer = task.freelancer
     mail(to: @client.email, subject: 'Changes Have Been Submitted for Your Task')
+  end
+
+  def changes_submitted_freelancer(task)
+    @task = task
+    @freelancer = task.freelancer
+    mail(to: @freelancer.email, subject: 'Changes Have Been Submitted for Task - #{@task.title}')
   end
 end
