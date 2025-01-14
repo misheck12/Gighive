203fe240b284b201276c92910b43d6bf6b98ee19
Misheck12 <Misheck1720@gmail.com>
2025-01-14 09:26:53 +0000

    Jan 14, 2025, 11:26 AM

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,11 +1,10 @@
 class PaymentsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_task, only: [:new, :create]
-  before_action :set_payment, only: [:accept, :reject, :show]
+  before_action :set_payment, only: [:accept, :reject]
 
   def new
     @payment = Payment.new
-    @budgets = Budget.all
   end
 
   def show
@@ -14,17 +13,16 @@ class PaymentsController < ApplicationController
       redirect_to root_path, alert: 'Payment not found'
     end
   end
+  
 
   def create
     @payment = @task.build_payment(payment_params)  # Use build_payment for has_one association
     @payment.user = current_user
-    @payment.client = @task.client # Ensure client is set correctly
     @payment.status = :pending  # Assuming the status enum includes pending
 
     if @payment.save
       redirect_to task_path(@task), notice: 'Payment was successfully submitted and is pending approval.'
     else
-      @budgets = Budget.all
       render :new
     end
   end
@@ -52,10 +50,10 @@ class PaymentsController < ApplicationController
   end
 
   def set_payment
-    @payment = Payment.find_by(id: params[:id])
+    @payment = Payment.find(params[:id])
   end
 
   def payment_params
-    params.require(:payment).permit(:amount, :transaction_id, :payment_proof, :network, :budget_id)
+    params.require(:payment).permit(:transaction_id, :payment_proof, :network)
   end
 end
